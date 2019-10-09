ac54e955dd729b3257c78db4c43740f73cccb903
朱雪飞 <zhuxuefei@trendytech.com.cn>
2019-10-09 18:24:03 +0800

    2019-10-09优化读取excel测试用例，不约束具体的接口参数字段和长度；调通添加资源池接口(解决多层次请求body数据)

--- Common/get_excel_data.py
+++ Common/get_excel_data.py
@@ -3,27 +3,64 @@
 # @Author  : zhuxuefei
 
 import xlrd
+import openpyxl
 
 class OperationExcleData():
     """对excel进行操作，包括读取请求参数，和填写操作结果"""
-    def __init__(self, excelFile):
+    def __init__(self, excelFile, sheetName):
+        self.sheetName = sheetName
         self.excelFile = excelFile
         self.caseList = []
 
-    """传入excel用例名称，其中元素是{}，每个{}包含字段和对应值的键对值"""
-    def getCaseList(self, excelFile):
+    def getCaseList(self, excelFile, sheetName):
+        """
+        获取excel中维护的测试用例，每个{}包含字段和对应值的键对值
+        :param excelFile: 传入excel文件的名字
+        :param sheetName: 传入接口所在sheet页的名字
+        :return: 最终返回所有的测试用例
+        """
         readExcel = xlrd.open_workbook(excelFile)
-        # 获取excel的第一个sheet
-        sheet = readExcel.sheet_by_index(0)
-        print(sheet.name)
-        # 获取excel的行数
+        sheet = readExcel.sheet_by_name(sheetName)
+        # 获取excel的行数和列数
         trows = sheet.nrows
-        for n in range(1, trows):
+        tcols = sheet.ncols
+        for i in range(2, trows):
             tmpdict = {}
-            tmpdict["name"] = sheet.cell_value(n, 2)
-            tmpdict["description"] = sheet.cell_value(n, 3)
+            for j in range(2, tcols):
+                case_key = sheet.cell_value(0, j)
+                case_value = sheet.cell_value(i, j)
+                if sheet.cell_value(1, j) == "datacenter":
+                    childdict = {}
+                    childcase_key = sheet.cell_value(1, j)
+                    childdict[childcase_key] = sheet.cell_value(i, j)
+                    case_value = childdict
+                tmpdict[case_key] = case_value
             self.caseList.append(tmpdict)
         return self.caseList
+
+
+    def writeExcel(self, excelFile, sheetName, writeData):
+        writeExcel = openpyxl.load_workbook(excelFile)
+        try:
+            writeSheet = writeExcel[sheetName]
+            writeSheet["C2"]=writeData
+            writeExcel.save(excelFile)
+        except Exception as e:
+            raise
+        finally:
+            pass
+
+if __name__=="__main__":
+    excelFile = "E:\\AutomationTestTask\\Test_data\\计算资源\\资源池.xlsx"
+    sheetName = "添加资源池"
+    ed = OperationExcleData(excelFile, sheetName)
+    resourcepool_data = ed.getCaseList(excelFile, sheetName)
+    print(resourcepool_data)
+
+
+
+
+
 
 
 
--- cmp/test_resourcepool.py
+++ cmp/test_resourcepool.py
@@ -1,139 +1,29 @@
 import pytest
 import requests
+import json
+from Common.get_excel_data import OperationExcleData
+from cmp.test_datacenter import test_getDataCenterId
 
-creat_url_path = "/v1/resourcepools"
-#check_url = "/v1/resourcepools/{resourcePoolId}"
-#update_url = "/v1/resourcepools/{resourcePoolId}"
-connect_url = "/v1/resourcepools/connecttesting"
-update_password_url = "/v1/resourcepools/{resourcePoolId}/password"
+# 添加资源池请求url
+createResourcePool_url = "/admin/v1/resourcepools"
 
-param = [{"region": 27287673.829845935,
-  "name": "cillum",
-  "type": "irure est aute",
-  "ip": "sit laboris id aliqua cill",
-  "port": 38639636.718895346,
-  "username": "",
-  "password": "dolor",
-  "extra": {
-    "datacenter": "esse officia occaecat et velit",
-    "region": "qui nostrud",
-    "protocal": "nulla sed tempor occaecat",
-    "domain": "cupidatat"}}
-]
+excelFile = "E:\\AutomationTestTask\\Test_data\\计算资源\\资源池.xlsx"
+sheetName = "添加资源池"
+resourcepool_data = OperationExcleData(excelFile, sheetName).getCaseList(excelFile, sheetName)
 
-update_param = [{
-  "region": 1,
-  "name": "deserunt enim",
-  "description": "sed ullamco occaecat",
-  "ip": "adipisicing in anim aute",
-  "port": 15445532.754962876,
-  "proxyIp": "consequat dolore velit reprehenderit Ut",
-  "proxyPort": -91655201.5879195,
-  "username": "sit Excepteur ut voluptate qui"
-}]
+def getDataCenterRegion(ip, port, headers):
+    regionID = test_getDataCenterId(ip, port, headers)
+    OperationExcleData(excelFile, sheetName).writeExcel(excelFile, sheetName, regionID)
 
-update_password = [{
-  "password": "ut"
-}]
-
-connect=[{
-  "type": "aute qui aliqua",
-  "ip": "inci",
-  "port": -69663434.88071649,
-  "username": "est",
-  "password": "esse dolor dolore est consequat",
-  "extra": {}
-}
-]
-
-@pytest.mark.parametrize("createparam",param)
-@pytest.mark.parametrize("updateparam",update_param)
-
-def test_resourcepool(ip, port, createparam,updateparam, headers):
+@pytest.mark.parametrize("resourcepool_data", resourcepool_data)
+def test_createResourcePool(ip, port, headers, resourcepool_data):
     ip_address = "http://%s:%s" % (ip, port)
-
-    print(createparam)
-    # query_values = createparam['query_strings']
-    # payload = createparam['payload']
-    #response_status_code = createparam['response_status_code']
-    #response_payload_status = createparam['response_payload_status']
-    # response_payload_snippets = createparam['response_payload_snippets']
-
-    # 创建资源池
-    create_post_response = requests.post(url=ip_address + creat_url_path,
-                                  params=createparam,
-                                  headers=headers).json()
-    code = create_post_response["data"]["status"]
-    resource_id = create_post_response["data"]["id"]
-
-    #print(create_post_response)
-    try:
-        assert code == 200#response_status_code
-    except Exception as e:
-        raise e
-    #return resource_id
-    # 查询资源池
-    check_url = "/v1/resourcepools/%resourceId" %resource_id
-    check_response = requests.get(url=ip_address + check_url,
-                                  headers = headers).json()
-    code = check_response["data"]["status"]
-    #print(check_response)
-    try:
-        assert code == 200#response_status_code
-    except Exception as e:
-        raise e
-
-    #修改资源池
-    update_response = requests.put(url=ip_address + check_url,
-                                   params = updateparam,
-                                  headers = headers).json()
-    #code = update_response["data"]["status"]
-    name = updateparam["name"]
-    check_response = requests.get(url=ip_address + check_url,
-                                  headers=headers).json()
-    code = check_response["data"]["status"]
-    update_name = check_response["data"]["name"]#修改名称
-    print(update_response)
-    try:
-        assert code == 200 # response_status_code
-        assert update_name == name
-    except Exception as e:
-        raise e
-
-    # 修改资源池密码
-    update_password_url = "/v1/resourcepools/ %resource_id/password" %resource_id
-    update_response = requests.put(url=ip_address + update_password_url,
-                                       params=updateparam,
-                                       headers=headers).json()
-    # code = update_response["data"]["status"]
-    name = updateparam["name"]
-    check_response = requests.get(url=ip_address + check_url,
-                                      headers=headers).json()
-    code = check_response["data"]["status"]
-    update_name = check_response["data"]["name"]  # 修改名称
-    print(update_response)
-    try:
-        assert code == 200  # response_status_code
-        assert update_name == name
-    except Exception as e:
-        raise e
-
-
-#查询资源池连接
-@pytest.mark.parametrize("connectparam", connect)
-def test_connect(ip, port, connectparam, headers):
-    ip_address = "http://%s:%s" % (ip, port)
-
-
-    connect_response = requests.put(url=ip_address + connect_url,
-                                   params=connectparam,
-                                   headers=headers).json()
-    code = connect_response["data"]["status"]
-    try:
-        assert code == 200 # response_status_code
-    except Exception as e:
-        raise e
-
+    createResourcePool_response = requests.post(url=ip_address + createResourcePool_url,
+                                              data=json.dumps(resourcepool_data),
+                                              headers=headers)
+    code = createResourcePool_response.status_code
+    assert code == 200
+    print(createResourcePool_response.text)
 
 
 
