dddd9f2b3d06212f3eaa1044c2389fefa44cf9b7
杜翔宇 <duxiangyu@trendytech.com.cn>
2019-10-11 14:59:01 +0800

    Default Changelist

--- network_resource/test_vlanpool.py
+++ network_resource/test_vlanpool.py
@@ -1,20 +1,24 @@
 import pytest
 import requests
-from network_resource.conftest import read_excel_tuple, read_excel_dic, write_excel
+from network_resource.conftest import read_excel,read_excel_tuple, read_excel_dic, write_excel
 
 # 路径
 ## 创建VLAN池
-create_vlanpool_url_path = '/admin/v1/vlan_pools'
+create_vlanpool_url_path = '/admin/v1/vlan_pools/'
 ## 获取VLAN池
 get_vlanpool_url_path = '/admin/v1/vlan_pools/page?tag=TRADITIONAL_VLAN'
 ## 编辑VLAN池
-update_vlanpool_url_path = '/admin/v1/vlan_pools'
+update_vlanpool_url_path = '/admin/v1/vlan_pools/'
+## 删除VLAN池
+delete_vlanpool_url_path = '/admin/v1/vlan_pools/'
 
 # 构造测试数据
 ## 创建VLAN池
 param_create_vlanpool = read_excel_tuple('测试数据.xlsx', '创建VLAN池')
 ## 编辑VLAN池
 param_update_vlanpool = read_excel_tuple('测试数据.xlsx', '编辑VLAN池')
+## 删除VLAN池
+param_delete_vlanpool = read_excel('测试数据.xlsx', '删除VLAN池','name')
 
 
 # 定义函数
@@ -58,10 +62,11 @@ def test_create_vlanpool(ip, port, headers, name, tag, vlanPoolResourcePoolList,
         'vlanTagEnd': vlanTagStart,  # 结束VLAN ID
         'vlanTagStart': vlanTagEnd  # 起始VLAN ID
     }
-    create_vlanpool_response = requests.post(url=ip_address + create_vlanpool_url_path,
-                                             headers=headers,
-                                             json=param
-                                             ).json()
+    create_vlanpool_response = requests.post(
+        url=ip_address + create_vlanpool_url_path,
+        headers=headers,
+        json=param
+    ).json()
     print(create_vlanpool_response)
     code = create_vlanpool_response['status']
     assert code == 200
@@ -82,24 +87,31 @@ def test_update_vlanpool(ip, port, headers, name, update_name, tag, vlanPoolReso
         'vlanTagEnd': vlanTagEnd
     }
     update_vlanpool_response = requests.put(
-        url=ip_address + create_vlanpool_url_path + "/" + str(vlanpool_id),
+        url=ip_address + update_vlanpool_url_path + str(vlanpool_id),
         headers=headers,
         json=param
     ).json()
-    code = update_vlanpool_response["status"]
+    code = update_vlanpool_response['status']
     assert code == 200
-    assert param_updatevlanpool["name"] in get_vlanpool_name_list(ip, port, headers)
+    assert name not in get_vlanpool_name_list(ip, port, headers)
+    assert update_name in get_vlanpool_name_list(ip, port, headers)
 
 
-# # 删除VLAN池
-# def test_deletevlanpool(ip,port,headers):
-#     ip_address = "http://%s:%s" % (ip,port)
-#     vlanpool_id = get_vlanpool_id(ip,port,headers,updatename)
-#     deletevlanpool_response = requests.delete(url=ip_address + create_vlanpool_url_path + "/" + str(vlanpool_id[0]),
-#                                      headers=headers).json()
-#     code =deletevlanpool_response["status"]
-#     assert code == 200
-#     assert updatename not in get_vlanpool_name_list(ip,port,headers)
-#
+## 删除VLAN池
+@pytest.mark.parametrize('name', param_delete_vlanpool)
+def test_delete_vlanpool(ip, port, headers, name):
+    ip_address = 'http://%s:%s' % (ip, port)
+    print(name)
+    vlanpool_id = get_vlanpool_id(ip, port, headers, name)[0]
+    print(get_vlanpool_id(ip, port, headers, name))
+    delete_vlanpool_response = requests.delete(
+        url=ip_address + delete_vlanpool_url_path + str(vlanpool_id),
+        headers=headers
+    ).json()
+    code = delete_vlanpool_response['status']
+    assert code == 200
+    assert name not in get_vlanpool_name_list(ip, port, headers)
+
+
 if __name__ == '__main__':
     pytest.main()
