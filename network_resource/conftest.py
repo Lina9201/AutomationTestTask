4c759654aae555ff4f2276a4a7d30108f9286edf
杜翔宇 <duxiangyu@trendytech.com.cn>
2019-10-11 14:37:36 +0800

    Default Changelist

--- network_resource/conftest.py
+++ network_resource/conftest.py
@@ -55,8 +55,9 @@ def headers(token):
     }
     return headers
 
+
 # 读取工作簿，表，列，该列以列表形式展示['A2', 'A3', 'A4', 'A5', 'A6']
-def read_excel(bookname, sheetname,columnname):
+def read_excel(bookname, sheetname, columnname):
     # 打开已存在的workbook
     wb = load_workbook(bookname)
     # 通过sheet名打开sheet页
@@ -66,13 +67,14 @@ def read_excel(bookname, sheetname,columnname):
     columns = ws.max_column
     # print(list(ws.columns))
     for i in range(columns):
-        a= list(ws.columns)[i]
+        a = list(ws.columns)[i]
         # print(a)
         b = []
         if columnname == a[0].value:
             for j in range(1, rows):
                 b.append(a[j].value)
             return (b)
+
 
 # 读取工作簿，表，以列表中元组的形式展示[('A2', 'B2', 'C2'), ('A3', 'B3', 'C3'), ('A4', 'B4', 'C4')]
 def read_excel_tuple(bookname, sheetname):
@@ -87,7 +89,7 @@ def read_excel_tuple(bookname, sheetname):
         print('没数据')
     else:
         param = []
-        for i in range(1,rows):
+        for i in range(1, rows):
             paramrow = list(ws.rows)[i]
             paramrow_value = []
             for j in range(columns):
@@ -95,6 +97,7 @@ def read_excel_tuple(bookname, sheetname):
             paramrow_value_tuple = tuple(paramrow_value)
             param.append(paramrow_value_tuple)
         return (param)
+
 
 # 读取工作簿，表，以列表中字典的形式展示[{'A1': 'A2', 'B1': 'B2', 'C1': 'C2'}, {'A1': 'A3', 'B1': 'B3', 'C1': 'C3'}]
 def read_excel_dic(bookname, sheetname):
--- network_resource/test_vlanpool.py
+++ network_resource/test_vlanpool.py
@@ -1,83 +1,96 @@
 import pytest
 import requests
-from network_resource.conftest import read_excel_tuple,read_excel_dic, write_excel
+from network_resource.conftest import read_excel_tuple, read_excel_dic, write_excel
 
 # 路径
 ## 创建VLAN池
 create_vlanpool_url_path = '/admin/v1/vlan_pools'
-# ## 获取VLAN池
-# get_vlanpool_url_path = "/admin/v1/vlan_pools/page?tag=TRADITIONAL_VLAN"
-#
-# 构造测试数据
-param_create_vlanpool = read_excel_tuple('测试数据.xlsx','创建VLAN池')
-# # updatename = "自动创建VLAN池修改"
-# #
-# # 定义函数
-# ## 获取VLAN池列表
-# def get_vlanpool_list(ip,port,headers):
-#     ip_address = "http://%s:%s" % (ip,port)
-#     vlanpoollist_response = requests.get(url=ip_address + get_vlanpool_url_path,
-#                                          headers=headers).json()
-#     vlanpoollist = vlanpoollist_response["data"]["list"]
-#     return vlanpoollist
-#
-# ## 获取VLAN池名称
-# def get_vlanpool_name_list(ip,port,headers):
-#     vlanpool_name_list = []
-#     for i in get_vlanpool_list(ip,port,headers):
-#         vlanpool_name_list.append(i["name"])
-#     return vlanpool_name_list
+## 获取VLAN池
+get_vlanpool_url_path = '/admin/v1/vlan_pools/page?tag=TRADITIONAL_VLAN'
+## 编辑VLAN池
+update_vlanpool_url_path = '/admin/v1/vlan_pools'
 
-# ## 获取VLAN池ID
-# def get_vlanpool_id (ip,port,headers,name):
-#     vlanpool_id = []
-#     for i in get_vlanpool_list(ip,port,headers):
-#         if name == i["name"]:
-#             vlanpool_id.append(i["id"])
-#     return vlanpool_id
-#
+# 构造测试数据
+## 创建VLAN池
+param_create_vlanpool = read_excel_tuple('测试数据.xlsx', '创建VLAN池')
+## 编辑VLAN池
+param_update_vlanpool = read_excel_tuple('测试数据.xlsx', '编辑VLAN池')
+
+
+# 定义函数
+## 获取VLAN池列表
+def get_vlanpool_list(ip, port, headers):
+    ip_address = 'http://%s:%s' % (ip, port)
+    vlanpool_list_response = requests.get(
+        url=ip_address + get_vlanpool_url_path,
+        headers=headers
+    ).json()
+    vlanpool_list = vlanpool_list_response['data']['list']
+    return vlanpool_list
+
+
+## 获取VLAN池名称
+def get_vlanpool_name_list(ip, port, headers):
+    vlanpool_name_list = []
+    for i in get_vlanpool_list(ip, port, headers):
+        vlanpool_name_list.append(i['name'])
+    return vlanpool_name_list
+
+
+## 获取VLAN池ID
+def get_vlanpool_id(ip, port, headers, name):
+    vlanpool_id = []
+    for i in get_vlanpool_list(ip, port, headers):
+        if name == i['name']:
+            vlanpool_id.append(i['id'])
+    return vlanpool_id
+
 
 # 测试用例
 ## 创建VLAN池
-@pytest.mark.parametrize('name,tag,vlanPoolResourcePoolList,vlanTagStart,vlanTagEnd',param_create_vlanpool)
-def test_create_vlanpool(ip,port,headers,name,tag,vlanPoolResourcePoolList,vlanTagStart,vlanTagEnd):
-    ip_address = "http://%s:%s" % (ip,port)
+@pytest.mark.parametrize('name,tag,vlanPoolResourcePoolList,vlanTagStart,vlanTagEnd', param_create_vlanpool)
+def test_create_vlanpool(ip, port, headers, name, tag, vlanPoolResourcePoolList, vlanTagStart, vlanTagEnd):
+    ip_address = "http://%s:%s" % (ip, port)
     param = {
-        "name": name,    #VLAN池名称
-        "tag": tag,
-        "vlanPoolResourcePoolList": [{"resourcePoolId": 114}],      #作用域调用资源池ID
-        "vlanTagEnd": vlanTagStart,      #结束VLAN ID
-        "vlanTagStart": vlanTagEnd     #起始VLAN ID
+        'name': name,  # VLAN池名称
+        'tag': tag,
+        'vlanPoolResourcePoolList': [{'resourcePoolId': 114}],  # 作用域调用资源池ID
+        'vlanTagEnd': vlanTagStart,  # 结束VLAN ID
+        'vlanTagStart': vlanTagEnd  # 起始VLAN ID
     }
-    createvlanpool_response = requests.post(url = ip_address + create_vlanpool_url_path,
-                                            headers = headers,
-                                            json = param
-                                            ).json()
-    code = createvlanpool_response["status"]
+    create_vlanpool_response = requests.post(url=ip_address + create_vlanpool_url_path,
+                                             headers=headers,
+                                             json=param
+                                             ).json()
+    print(create_vlanpool_response)
+    code = create_vlanpool_response['status']
     assert code == 200
-    # assert name in get_vlanpool_name_list(ip,port,headers)
-#
-# # 编辑VLAN池
-# def test_update_vlanpool(ip,port,headers):
-#     ip_address = "http://%s:%s" % (ip,port)
-#     vlanpool_id = get_vlanpool_id(ip,port,headers,param_vlanpool["name"])[0]
-#     param_updatevlanpool = {
-#         "id": vlanpool_id,
-#         "name": updatename,
-#         "tag": "TRADITIONAL_VLAN",
-#         "vlanPoolResourcePoolList": [{"resourcePoolId": 114, "vlanPoolId":vlanpool_id}],
-#         "vlanTagEnd": 101,
-#         "vlanTagStart": 101
-#     }
-#     update_vlanpool_response = requests.put(url=ip_address + create_vlanpool_url_path + "/" + str(vlanpool_id),
-#                                             headers=headers,
-#                                             json=param_updatevlanpool
-#                                           ).json()
-#     code = update_vlanpool_response["status"]
-#     assert code == 200
-#     assert param_updatevlanpool["name"] in get_vlanpool_name_list(ip,port,headers)
-#
-#
+    assert name in get_vlanpool_name_list(ip, port, headers)
+
+
+## 编辑VLAN池
+@pytest.mark.parametrize('name,update_name,tag,vlanPoolResourcePoolList,vlanTagStart,vlanTagEnd', param_update_vlanpool)
+def test_update_vlanpool(ip, port, headers, name, update_name, tag, vlanPoolResourcePoolList, vlanTagStart, vlanTagEnd):
+    ip_address = 'http://%s:%s' % (ip, port)
+    vlanpool_id = get_vlanpool_id(ip, port, headers, name)[0]
+    param = {
+        'id': vlanpool_id,
+        'name': update_name,
+        'tag': tag,
+        'vlanPoolResourcePoolList': [{'resourcePoolId': 114, 'vlanPoolId': vlanpool_id}],
+        'vlanTagStart': vlanTagStart,
+        'vlanTagEnd': vlanTagEnd
+    }
+    update_vlanpool_response = requests.put(
+        url=ip_address + create_vlanpool_url_path + "/" + str(vlanpool_id),
+        headers=headers,
+        json=param
+    ).json()
+    code = update_vlanpool_response["status"]
+    assert code == 200
+    assert param_updatevlanpool["name"] in get_vlanpool_name_list(ip, port, headers)
+
+
 # # 删除VLAN池
 # def test_deletevlanpool(ip,port,headers):
 #     ip_address = "http://%s:%s" % (ip,port)
@@ -88,3 +101,5 @@ def test_create_vlanpool(ip,port,headers,name,tag,vlanPoolResourcePoolList,vlanT
 #     assert code == 200
 #     assert updatename not in get_vlanpool_name_list(ip,port,headers)
 #
+if __name__ == '__main__':
+    pytest.main()
