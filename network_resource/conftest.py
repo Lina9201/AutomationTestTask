a6aae85a96b15855c758c0fb2dcc595fb1d76c7f
杜翔宇 <duxiangyu@trendytech.com.cn>
2019-10-11 11:28:45 +0800

    Default Changelist

--- network_resource/conftest.py
+++ network_resource/conftest.py
@@ -74,6 +74,27 @@ def read_excel(bookname, sheetname,columnname):
                 b.append(a[j].value)
             return (b)
 
+# 读取工作簿，表，以字典的形式展示
+def read_excel_tuple(bookname, sheetname):
+    # 打开已存在的workbook
+    wb = load_workbook(bookname)
+    # 通过sheet名打开sheet页
+    ws = wb[sheetname]
+    # 获取sheet页最大行数，最大列数
+    rows = ws.max_row
+    columns = ws.max_column
+    if rows <= 1:
+        print('没数据')
+    else:
+        param = []
+        for i in range(1,rows):
+            paramrow = list(ws.rows)[i]
+            paramrow_value = []
+            for j in range(columns):
+                paramrow_value.append(paramrow[j].value)
+            paramrow_value_tuple = tuple(paramrow_value)
+            param.append(paramrow_value_tuple)
+        return (param)
 
 # 读取工作簿，表，以字典的形式展示
 def read_excel_dic(bookname, sheetname):
