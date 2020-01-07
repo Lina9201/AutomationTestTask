2845e047ef4115372fcd27f2c29db48ce57721db
朱雪飞 <zhuxuefei@trendytech.com.cn>
2020-01-07 14:01:54 +0800

    1、python连接mysql.arangodb公共方法 2、创建编辑删除配置项类型

--- common/get_db_data.py
+++ common/get_db_data.py
@@ -3,11 +3,12 @@
 # @Author  : zhuxuefei
 from config.Conf import ConfigYaml
 from utils.MysqlUtil import MysqlUtil
+from utils.ArangodbUtil import ArangodbUtil
 from utils.AssertUtil import AssertUtil
 
-def init_db(db_alias):
+def init_mysqldb(db_alias):
     # 从配置文件读取数据库信息进行初始化
-    db_info = ConfigYaml.get_db_config(db_alias)
+    db_info = ConfigYaml().get_db_config(db_alias)
     db_host = db_info["db_host"]
     db_user = db_info["db_user"]
     db_password = db_info["db_password"]
@@ -18,13 +19,23 @@ def init_db(db_alias):
     conn = MysqlUtil(db_host, db_user, db_password, db_name, db_charset, db_port)
     return conn
 
+def init_arangodb(db_alias):
+    #从配置文件中读取Arangodb数据库信息进行初始化
+    arangodb_info = ConfigYaml().get_db_config(db_alias)
+    arangoURL = arangodb_info['arangoURL']
+    username = arangodb_info['username']
+    password = arangodb_info['password']
 
-def assert_db(db_name,result,db_verify):
+    conn = ArangodbUtil(arangoURL, username,password)
+    return conn
+
+def assert_mysqldb(db_name,result,db_verify):
     assert_util = AssertUtil()
-    sql = init_db(db_name)
+    sql = init_mysqldb(db_name)
     db_res = sql.fetchone(db_verify)
     verify_list = list(dict(db_res).keys())
     for line in verify_list:
         res_line = result[line]
         res_db_line = dict(db_res)[line]
         assert_util.assert_body(res_line, res_db_line)
+
--- utils/MysqlUtil.py
+++ utils/MysqlUtil.py
@@ -5,19 +5,17 @@
 import pymysql
 
 class MysqlUtil:
-    def __init__(self, host, username, password, database, charset="utf8", port=3306):
+    def __init__(self,host,user,password,database,charset="utf8",port=3306):
+        self.conn = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+            charset=charset,
+            port=port
+        )
+        self.cursor = self.conn.cursor(cursor=pymysql.cursors.DictCursor)
 
-            # 连接数据库信息
-            self.conn = pymysql.connect(
-                host = host,
-                username = username,
-                password = password,
-                database = database,
-                charset = charset,
-                port = port
-            )
-            # 创建光标对象
-            self.cursor = self.conn.cursor()
 
     # 创建查询、执行方法
     def fetchone(self, sql):
@@ -62,5 +60,15 @@ class MysqlUtil:
         # 关闭连接对象
         if self.conn is not None:
             self.conn.close()
+
+if __name__ == "__main__":
+    mysql = MysqlUtil("172.50.10.41",
+                  "root",
+                  "root1234",
+                  "cmp_compute",
+                  charset="utf8",
+                  port=3306)
+    res = mysql.exec("select * from instance")
+    print(res)
 
 
